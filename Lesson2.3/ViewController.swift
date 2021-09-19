b5c91ac0f38bb3e9e7392c91b081671526eaba5a
bobdubei <vovandv@gmail.com>
2021-09-19 22:08:52 +0300

    v1.1

--- Lesson2.3/ViewController.swift
+++ Lesson2.3/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var minRange: UILabel!
+    @IBOutlet var maxRange: UILabel!
+    
+    @IBOutlet var resultRandom: UILabel!
+    
+    @IBOutlet var getResultButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getResultButton.layer.cornerRadius = 10
+      
     }
 
+    
+    @IBAction func getResultAction() {
+        let minValue = Int(minRange.text ?? "") ?? 0
+        let maxValue = Int(maxRange.text ?? "") ?? 100
+        
+        resultRandom.text = String(Int.random(in: minValue...maxValue))
+    }
+    
 
 }
 
